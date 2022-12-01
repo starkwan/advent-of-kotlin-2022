96bc8e52929b5f224dc7cbe068500ec8a3f850d8
starkwan <starkwan@users.noreply.github.com>
2022-12-01 16:31:22 +0000

    AoC Day1

--- calendar/day01/Day1.kt
+++ calendar/day01/Day1.kt
@@ -5,10 +5,18 @@ import Lines
 
 class Day1 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.joinToString("\n") // rawText
+            .split("\n\n")
+            .map { it.split("\n").sumOf { it.toInt() } }
+            .max()
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.joinToString("\n") // rawText
+            .split("\n\n")
+            .map { it.split("\n").sumOf { it.toInt() } }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 }
