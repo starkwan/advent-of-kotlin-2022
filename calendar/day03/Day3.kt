42a68ea508ddcc4dd947a09e3057471d216452c1
starkwan <starkwan@users.noreply.github.com>
2022-12-03 09:36:46 +0000

    AoC Day3 - bugfix

--- calendar/day03/Day3.kt
+++ calendar/day03/Day3.kt
@@ -6,21 +6,22 @@ import Lines
 class Day3 : Day() {
     override fun part1(input: Lines): Any {
         return input
-            .map {
-                it.chunked(it.length / 2)
+            .map { rucksack ->
+                rucksack.chunked(rucksack.length / 2)
                     .map { it.toSet() }
                     .let { it[0].intersect(it[1]).iterator().next() }
             }
-            .sumOf { if (it > 'a') it - 'a' + 1 else it - 'A' + 27 }
+            .sumOf { if (it >= 'a') it - 'a' + 1 else it - 'A' + 27 }
     }
 
     override fun part2(input: Lines): Any {
-        return input.chunked(3)
+        return input
+            .chunked(3)
             .map { rucksacks ->
                 rucksacks.map { it.toSet() }
                     .reduce { acc, rucksack -> acc.intersect(rucksack) }
             }
             .flatten()
-            .sumOf { if (it > 'a') it - 'a' + 1 else it - 'A' + 27 }
+            .sumOf { if (it >= 'a') it - 'a' + 1 else it - 'A' + 27 }
     }
 }
