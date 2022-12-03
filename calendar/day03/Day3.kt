e2d71f89d9adfe9f48ddc4b2f1a5bca371940ebf
starkwan <starkwan@users.noreply.github.com>
2022-12-03 09:27:56 +0000

    AoC Day3

--- calendar/day03/Day3.kt
+++ calendar/day03/Day3.kt
@@ -5,10 +5,22 @@ import Lines
 
 class Day3 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        return input
+            .map {
+                it.chunked(it.length / 2)
+                    .map { it.toSet() }
+                    .let { it[0].intersect(it[1]).iterator().next() }
+            }
+            .sumOf { if (it > 'a') it - 'a' + 1 else it - 'A' + 27 }
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input.chunked(3)
+            .map { rucksacks ->
+                rucksacks.map { it.toSet() }
+                    .reduce { acc, rucksack -> acc.intersect(rucksack) }
+            }
+            .flatten()
+            .sumOf { if (it > 'a') it - 'a' + 1 else it - 'A' + 27 }
     }
 }
