ea50e5d1efc828092fe5f712f89bedf134ba428e
starkwan <starkwan@users.noreply.github.com>
2022-12-06 07:51:59 +0000

    AoC Day6

--- calendar/day06/Day6.kt
+++ calendar/day06/Day6.kt
@@ -5,10 +5,24 @@ import Lines
 
 class Day6 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        val buffer = mutableListOf(' ', ' ', ' ', ' ')
+        input.single().toList().forEachIndexed { i, char ->
+            buffer[i % buffer.size] = char
+            if (i >= buffer.size - 1 && buffer.toSet().size == buffer.size) {
+                return i + 1
+            }
+        }
+        error("Not found")
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        val buffer = mutableListOf<Char>().apply { repeat(14) { this.add(' ') } }
+        input.single().toList().forEachIndexed { i, char ->
+            buffer[i % buffer.size] = char
+            if (i >= buffer.size - 1 && buffer.toSet().size == buffer.size) {
+                return i + 1
+            }
+        }
+        error("Not found")
     }
 }
