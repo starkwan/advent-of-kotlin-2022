13fb66c6b98d452e0ebfc5440b0cd283f8b7c352
starkwan <starkwan@users.noreply.github.com>
2022-12-15 15:18:57 +0000

    AoC Day15

--- calendar/day15/Day15.kt
+++ calendar/day15/Day15.kt
@@ -2,13 +2,87 @@ package day15
 
 import Day
 import Lines
+import kotlin.math.abs
 
 class Day15 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        val targetY = 2000000
+        val sensorsAtTargetY = mutableSetOf<Int>()
+        val beaconsAtTargetY = mutableSetOf<Int>()
+        val infeasibleAtTargetY = mutableSetOf<Int>()
+        val regex = """Sensor at x=([-]?\d+), y=([-]?\d+): closest beacon is at x=([-]?\d+), y=([-]?\d+)""".toRegex()
+        for (line in input) {
+            regex.findAll(line)
+                .map { result -> result.groups.drop(1).map { it!!.value.toInt() } }
+                .forEach { (sx, sy, bx, by) ->
+                    if (sy == targetY) sensorsAtTargetY.add(sx)
+                    if (by == targetY) beaconsAtTargetY.add(bx)
+                    val mDistance = abs(sy - by) + abs(sx - bx)
+                    val mDistanceAtTargetY = mDistance - abs(sy - targetY)
+                    if (mDistanceAtTargetY >= 0) {
+                        for (step in 0..mDistanceAtTargetY) {
+                            infeasibleAtTargetY.add(sx - step)
+                            infeasibleAtTargetY.add(sx + step)
+                        }
+                    }
+                }
+        }
+        infeasibleAtTargetY.removeAll(sensorsAtTargetY + beaconsAtTargetY)
+        return infeasibleAtTargetY.size
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        val env = Environment(input)
+        return env.findBeacon().let { it.x.toLong() * 4000000 + it.y.toLong() }
     }
+
+    class Environment(input: Lines) {
+        private val sensors = mutableListOf<Cell>()
+        private val regex =
+            """Sensor at x=([-]?\d+), y=([-]?\d+): closest beacon is at x=([-]?\d+), y=([-]?\d+)""".toRegex()
+        private val min = 0
+        private val max = 4000000
+
+        init {
+            for (line in input) {
+                regex.findAll(line)
+                    .map { result -> result.groups.drop(1).map { it!!.value.toInt() } }
+                    .forEach { (sx, sy, bx, by) ->
+                        val mDistance = abs(sy - by) + abs(sx - bx)
+                        sensors.add(Cell(sx, sy, mDistance))
+                    }
+            }
+        }
+
+        fun findBeacon(): Cell {
+            for (y in 0..max) {
+                findBeaconAtY(y)?.let { return it }
+            }
+            error("failed to find")
+        }
+
+        private fun getCellAt(x: Int, y: Int): Cell {
+            val step = sensors.map { sensor ->
+                val mDistanceTraveled = abs(x - sensor.x) + abs(y - sensor.y)
+                sensor.steps - mDistanceTraveled // remaining steps at x,y
+            }.max()
+            return Cell(x, y, maxOf(-1, step))
+        }
+
+        private fun findBeaconAtY(y: Int): Cell? {
+            var cell = getCellAt(min, y)
+            while (cell.x <= max) {
+                if (cell.steps == -1) {
+                    return cell
+                }
+                cell = getCellAt(cell.x + cell.steps + 1, y)
+            }
+            return null
+        }
+    }
+
+    /**
+     * @param steps guaranteed no beacon can be found within number of steps(mDistance)
+     */
+    data class Cell(val x: Int, val y: Int, val steps: Int)
 }
